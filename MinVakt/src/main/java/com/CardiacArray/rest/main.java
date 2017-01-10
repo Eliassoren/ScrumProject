14e010dee19a0c65746bd26dfb5c81d840331d19
Vegard Stenvik <vegardks@gmail.com>
2017-01-10 13:33:37 +0100

    Maven-prosjekt satt opp

---
+++ MinVakt/src/main/java/com/CardiacArray/rest/main.java
@@ -0,0 +1,17 @@
+package com.CardiacArray.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * Created by Vegard on 10/01/2017.
+ */
+@Path("/")
+public class main {
+    @GET
+    @Produces("text/html")
+    public String getHello(){
+        return "Hello!";
+    }
+}
