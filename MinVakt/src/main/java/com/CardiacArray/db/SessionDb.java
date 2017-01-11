40375cd14472ffca35de7eed3e1692f216ccad17
Erik Kjosavik <erik_kjosavik@hotmail.com>
2017-01-11 12:26:33 +0100

    yes, yo

--- MinVakt/src/main/java/com/CardiacArray/db/SessionDb.java
+++ MinVakt/src/main/java/com/CardiacArray/db/SessionDb.java
@@ -1,7 +1,18 @@
 package com.CardiacArray.db;
 
+
+import java.sql.*;
+
 /**
  * Created by kjosavik on 11-Jan-17.
  */
 public class SessionDb {
+    private Connection connection;
+
+    public SessionDb(Connection connection){
+        connection = this.connection;
+    }
+
+    public 
+
 }
