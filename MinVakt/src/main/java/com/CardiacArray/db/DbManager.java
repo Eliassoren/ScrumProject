8b419ae74bba3c1169cef3af2d48fb74c76874b2
Erik Kjosavik <erik_kjosavik@hotmail.com>
2017-01-11 11:02:01 +0100

    endra litt på DbManager

--- MinVakt/src/main/java/com/CardiacArray/db/DbManager.java
+++ MinVakt/src/main/java/com/CardiacArray/db/DbManager.java
@@ -13,8 +13,6 @@ public class DbManager {
 
     public static Connection connection;
     private String dbDriver = "org.h2.Driver";
-    //private String dbLink = "jdbc:h2://mysql.stud.iie.ntnu.no/g_scrum01?user=g_scrum01&password=DarP7yTs";
-
 
     public DbManager() throws Exception{
         MysqlDataSource dataSource = new MysqlDataSource();
@@ -23,36 +21,18 @@ public class DbManager {
         dataSource.setPassword("");
         dataSource.setServerName("mysql.stud.iie.ntnu.no");
         Connection connection = dataSource.getConnection();
-        Statement statement = null;
-        ResultSet res = null;
-        ArrayList<String> al = new ArrayList<>();
 
-        String toSQL = "select * from user;";
         try{
             connection.setAutoCommit(false);
-            statement = connection.createStatement();
-            res = statement.executeQuery(toSQL);
-            connection.commit();
-
-            int tuppleIndex = 1;
-            while(res.next()){
-                System.out.println(res.getString(tuppleIndex));
-                tuppleIndex ++;
             }
 
         }catch (SQLException e){
             connection.rollback();
             e.printStackTrace(System.err);
         } finally {
-            res.close();
-            statement.close();
             connection.close();
         }
 
-    }
-
-    public static void establishConnection(Connection newConnection){
-        connection = newConnection;
     }
 
     public static void main(String[] args)throws Exception{
