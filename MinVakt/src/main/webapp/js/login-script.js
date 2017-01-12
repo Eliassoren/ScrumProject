ac9c3e46b9202aec06a9619387c43d7e6aac57a7
Roger Holten <roger.holten@gmail.com>
2017-01-12 13:18:48 +0100

    Login form validation logic.

--- MinVakt/src/main/webapp/js/login-script.js
+++ MinVakt/src/main/webapp/js/login-script.js
@@ -1,26 +1,18 @@
-/**
- * Created by Chris on 11.01.2017.
- */
-
-
-function login() {
-
-    var loginName = document.getElementById('uname').value;
-    var password = document.getElementById('password').value;
-
-    if (loginName == "Chris" && password == "123") {
-        location.href = 'http://google.com';
-    } else {
-        document.getElementById('errormessage').style.visibility = "initial";
-        setTimeout(errorMessageRemoval, 5000);
-    }
-}
-
-function errorMessageRemoval() {
-
-    document.getElementById('errormessage').style.visibility = "hidden";
-
-}
-/**
- * Created by Chris on 11.01.2017.
- */
+$(document).ready(function() {
+    $("#loginform").validate({
+        rules: {
+            username: "required",
+            password: {
+                required: true,
+                minlength: 8
+            }
+        },
+        messages: {
+            username: "Brukernavn er obligatorisk.",
+            password: {
+                required: "Passord er obligatorisk.",
+                minlength: "Passordet må bestå av minimum 8 tegn."
+            }
+        }
+    });
+});
