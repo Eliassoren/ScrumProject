a4fed64317803c87c5a5ffc0a3c9e4c782cc2a05
Odd Erik Knudsen <odderik.knudsen@hotmail.com>
2017-01-13 09:35:59 +0100

    Small changes

--- MinVakt/src/main/java/com/CardiacArray/rest/SessionService.java
+++ MinVakt/src/main/java/com/CardiacArray/rest/SessionService.java
@@ -8,26 +8,17 @@ package com.CardiacArray.rest;
 import com.CardiacArray.data.*;
 import com.CardiacArray.db.DbManager;
 import com.CardiacArray.db.SessionDb;
-import sun.security.krb5.Credentials;
-
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.Date;
-import java.util.Enumeration;
-import java.util.logging.Level;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 
@@ -88,95 +79,9 @@ public class SessionService {
     }
     
      public static void main(String[] args) throws Exception {
-          DbManager dbManager = new DbManager();
-         HttpSession hs = new HttpSession() {
-             @Override
-             public long getCreationTime() {
-                 return 0;
-             }
+        DbManager dbManager = new DbManager();
 
-             @Override
-             public String getId() {
-                 return null;
-             }
-
-             @Override
-             public long getLastAccessedTime() {
-                 return 0;
-             }
-
-             @Override
-             public ServletContext getServletContext() {
-                 return null;
-             }
-
-             @Override
-             public void setMaxInactiveInterval(int i) {
-
-             }
-
-             @Override
-             public int getMaxInactiveInterval() {
-                 return 0;
-             }
-
-             @Override
-             public HttpSessionContext getSessionContext() {
-                 return null;
-             }
-
-             @Override
-             public Object getAttribute(String s) {
-                 return null;
-             }
-
-             @Override
-             public Object getValue(String s) {
-                 return null;
-             }
-
-             @Override
-             public Enumeration<String> getAttributeNames() {
-                 return null;
-             }
-
-             @Override
-             public String[] getValueNames() {
-                 return new String[0];
-             }
-
-             @Override
-             public void setAttribute(String s, Object o) {
-
-             }
-
-             @Override
-             public void putValue(String s, Object o) {
-
-             }
-
-             @Override
-             public void removeAttribute(String s) {
-
-             }
-
-             @Override
-             public void removeValue(String s) {
-
-             }
-
-             @Override
-             public void invalidate() {
-
-             }
-
-             @Override
-             public boolean isNew() {
-                 return false;
-             }
-         };
-
-         SessionService sc = new SessionService(dbManager.connection);
+        SessionService sc = new SessionService(dbManager.connection);
         
         System.out.println(sc.login("epost@internett.no", "123", hs));
     }
