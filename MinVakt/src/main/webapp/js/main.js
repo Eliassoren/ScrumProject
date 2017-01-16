52b47d902fff9429eff470ccd663f97d55621777
Elias <eliassorr@gmail.com>
2017-01-16 11:33:56 +0100

    Added js for title of Calendar, applied font-awesome nav arrows instead of custom

--- MinVakt/src/main/webapp/js/main.js
+++ MinVakt/src/main/webapp/js/main.js
@@ -1,27 +1,54 @@
 /**
  * Created by EliasBrattli on 12/01/2017.
  */
+var day = new Date();
+var month = day.getMonth();
+var monthNames = new Array();
+var year = day.getYear();
+monthNames[0] = "Januar";
+monthNames[1] = "Februar";
+monthNames[2] = "Mars";
+monthNames[3] = "April";
+monthNames[4] = "Mai";
+monthNames[5] = "Juni";
+monthNames[6] = "Juli";
+monthNames[7] = "August";
+monthNames[8] = "September";
+monthNames[9] = "Oktober";
+monthNames[10] = "November";
+monthNames[11] = "Desember";
+
+var title = monthNames[month] +" "+ year;
 $(document).ready(function() {
     $("#hamburger-toggle").click(function(){
         $("#hamburger-menu").toggleClass("hamburger-menu-open");
         $("#hamburger-toggle").toggleClass("hamburger-toggle-open");
     });
+
+
+$("#month-title").textContent = title;
+
+    $("#left-arrow").click(function () {
+        alert("Clicked left");
+        if(month > 0){
+            month--;
+        }else{
+            year--;
+            month = 11;
+        }
+        title = monthNames[month] + " " + year;
+        $("#month-title").textContent = title;
+    });
+
+    $("#right-arrow").click(function () {
+        alert("Clicked right");
+        if(month < 11){
+            month++;
+        }else{
+            year++;
+            month = 0;
+        }
+        title = monthNames[month] + " " + year;
+        $("#month-title").textContent = title;
+    });
 });
-
-/*$('#triangle').mousedown(function(e) {
- e.preventDefault();
-
- //create matching canvas representation for the image
- if(!this.canvas) {
- this.canvas = $('<canvas/>')[0];
- this.canvas.width = this.width;
- this.canvas.height = this.height;
- this.canvas.getContext('2d').drawImage(this, 0, 0, this.width, this.height);
- }
- var pixelData = this.canvas.getContext('2d').getImageData(e.offsetX, e.offsetY, 1, 1).data;
-
- //check that the pixel is not transparent
- if (pixelData[3] > 0) {
- //your code
- }
- });*/
