c43407a6b53b8743019a1ba1618ca5bce3397f37
Christian Echtermeyer <chrech.birr@gmail.com>
2017-01-26 13:41:28 +0100

    some design and script fixed

--- MinVakt/src/main/webapp/js/overtime-script.js
+++ MinVakt/src/main/webapp/js/overtime-script.js
@@ -51,62 +51,43 @@ function addRow(data) {
 
     console.log(data);
 
-    //DUMMY DATA
-    //var text= '[{"shiftId":1,"startTime":1483254000000,"endTime":1483282800000,"userId":16,"userName":"Siri Sirisen","departmentId":1,"role":1,"tradeable":true,"responsibleUser":false},{"shiftId":6,"startTime":1483542000000,"endTime":1483570740000,"userId":16,"userName":"Siri Sirisen","departmentId":1,"role":1,"tradeable":false,"responsibleUser":false}]'
     var obj = data;
-
-    //TODO this needs to be removed after the table slector below works
-    var table = "dateNow-table";
-
 
     if (!document.getElementsByTagName) return;
 
     for (var i = 0; i < obj.length; i++) {
 
-        //TODO: This part needs to add to the correct table given a working time.
-
-        var startTime = new Date(obj[i].startTime).getHours();
-        var isFree = obj[i].tradeable;
-
-        if (startTime >= 8 && startTime < 16) {
-            table = "dateNow-table";
-        } else if (startTime >= 16 && startTime < 25) {
-            table = "evening-table";
-        } else if (startTime >= 0 && startTime < 8) {
-            table = "night-table";
-        } else {
-            alert("ERROR CHECK THE ADD ROW FUNCTION IN SHIFT-SCRIPT.JS!")
+        var hours = new Date(obj[i].endTime).getHours() - new Date(obj[i].startTime).getHours();
+        if (hours < 0){
+            hours = hours + 24;
         }
 
-        tabBody = document.getElementById(table);
+        tabBody = document.getElementById("overtime-table");
         row = document.createElement("tr");
         row.className = "tr" + i;
         cell1 = document.createElement("td");
         cell2 = document.createElement("td");
         cell3 = document.createElement("td");
-        cell4 = document.createElement("Button");
-        cell4.className = "listButton " + "id" + obj[i].shiftId;
-        textnode1 = document.createTextNode(obj[i].userName);
-        textnode2 = document.createTextNode(formatTime(startTime + ":" + new Date(obj[i].startTime).getMinutes()));
-        textnode3 = document.createTextNode(formatTime(new Date(obj[i].endTime).getHours() + ":" + new Date(obj[i].endTime).getMinutes()));
-        textnode4 = document.createTextNode("Ta vakt");
+        cell4 = document.createElement("td");
+        cell5 = document.createElement("Button");
+        cell5.className = "overtime-listButton " + "id" + obj[i].shiftId;
+        textnode1 = document.createTextNode(obj[i].shiftId);
+        textnode2 = document.createTextNode(obj[i].userName);
+        textnode3 = document.createTextNode(formatTime(new Date(obj[i].startTime).getHours() + ":" + new Date(obj[i].startTime).getMinutes()) + " - " + formatTime(new Date(obj[i].endTime).getHours() + ":" + new Date(obj[i].endTime).getMinutes()));
+        textnode4 = document.createTextNode(hours);
+        textnode5 = document.createTextNode("Godkjenn");
         cell1.appendChild(textnode1);
         cell2.appendChild(textnode2);
         cell3.appendChild(textnode3);
-        cell4.appendChild(textnode4)
+        cell4.appendChild(textnode4);
+        cell5.appendChild(textnode5);
         row.appendChild(cell1);
         row.appendChild(cell2);
         row.appendChild(cell3);
         row.appendChild(cell4);
+        row.appendChild(cell5);
         tabBody.appendChild(row);
         //table = "evening-table";
-        if (obj[i].userName == ""){
-            $('.tr' + i).css('background-color', '#FF5468');
-            //$('.id' + obj[i].shiftId).css('background-color', '#BA3E4C');
-        } else if (isFree) {
-            $('.tr' + i).css('background-color', '#4DFA90');
-            //$('.id' + obj[i].shiftId).css('background-color', '#40CD76');
-        }
     }
 }
 
