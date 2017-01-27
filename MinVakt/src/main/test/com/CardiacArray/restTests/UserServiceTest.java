88912672a6e6da80fce2ddca05866b35c37186b1
Vegard Stenvik <vegardks@gmail.com>
2017-01-27 17:05:57 +0100

    fixed tests

--- MinVakt/src/main/test/com/CardiacArray/restTests/UserServiceTest.java
+++ MinVakt/src/main/test/com/CardiacArray/restTests/UserServiceTest.java
@@ -5,6 +5,8 @@ import com.CardiacArray.restService.db.UserDb;
 import com.CardiacArray.restService.rest.UserService;
 import org.junit.Assert;
 import org.junit.Test;
+
+import javax.validation.constraints.AssertTrue;
 import javax.ws.rs.BadRequestException;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -61,18 +63,21 @@ public class UserServiceTest {
         UserService service = new UserService((mockUserDb));
         User validUser = new User("Ola", "Nordmann", 12345678, "test@test.no",
                 "Passord", false, "Testveien 2", 1, true, 100);
-        when(mockUserDb.getUserByEmail("test@test.no")).thenReturn(new User());
+        User returnUser = new User();
+        returnUser.setEmail("test@test.no");
+        when(mockUserDb.getUserByEmail("test@test.no")).thenReturn(null);
+        System.out.println(service.createUser(validUser).getStatus());
         Assert.assertTrue((service.createUser(validUser)).getStatus() == 200);
     }
 
-    @Test(expected = BadRequestException.class)
+    @Test
     public void createUserFailed() throws Exception {
         UserDb mockUserDb = mock(UserDb.class);
         UserService service = new UserService((mockUserDb));
         User invalidUser = new User("Ola", null, 12345678, "test@test.no",
                 "Passord", false, "Testveien 2", 1, true, 100);
         when(mockUserDb.getUserByEmail("test@test.no")).thenReturn(invalidUser);
-        service.createUser(invalidUser);
+        Assert.assertTrue((service.createUser(invalidUser)).getStatus() == 400);
     }
 
 }
