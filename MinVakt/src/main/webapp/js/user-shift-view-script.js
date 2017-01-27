5c283edaab64ab424a1e9f6dc78adb784fe81659
Christian Echtermeyer <chrech.birr@gmail.com>
2017-01-27 11:02:18 +0100

    small change to the script as it was using the wrong name for classes in the matchin css file

--- MinVakt/src/main/webapp/js/user-shift-view-script.js
+++ MinVakt/src/main/webapp/js/user-shift-view-script.js
@@ -21,8 +21,8 @@ function refreshTable() {
 }
 
 $(document).ready(function() {
-    $("#dropdown-toggle-dateNow").click(function(){
-        $("#dateNow").toggleClass("dropdown-active");
+    $("#dropdown-toggle-day").click(function(){
+        $("#day").toggleClass("dropdown-active");
     });
     $("#dropdown-toggle-evening").click(function(){
         $("#evening").toggleClass("dropdown-active");
@@ -67,7 +67,7 @@ function addRow(data) {
         var isFree = obj[i].tradeable;
 
         if (startTime >= 8 && startTime < 16) {
-            table = "dateNow-table";
+            table = "day-table";
         } else if (startTime >= 16 && startTime < 25) {
             table = "evening-table";
         } else if (startTime >= 0 && startTime < 8) {
